d55e56f629db30aba9cefcaabebcfb0b03ed3c7e
Jenny Medeiros <jennymedeiros@Jennys-MBP.medeiroshome>
2024-07-14 16:05:41 -0400

    Add tailwind. Start design conversion

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  darkMode: ['class', 'dark-mode'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
